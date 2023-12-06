146aa6dadbbaef3f403b99ff42061b04570c3039
ismailukman <ismailukman@gmail.com>
2023-12-06 18:04:44 -0500

    Adding all codes for figure reproduction

--- modularity_sci.m
+++ modularity_sci.m
@@ -5,8 +5,11 @@ gamma = 1.2; % gamma = 0.5:0.1:1.5
 log_omega = -1; % log_omega = 0:-1:-4
 omega = power(10,log_omega);        
 
-corr_g1 = permute(cell2mat(struct2cell(load('/Volumes/Elements/SCI/fc/corr_sci_c.mat'))), [2 3 1]);
-corr_g2 = permute(cell2mat(struct2cell(load('/Volumes/Elements/SCI/fc/corr_sci_t.mat'))), [2 3 1]);
+corr_g1 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_hc.mat'))), [2 3 1]);
+corr_g2 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_sci.mat'))), [2 3 1]);
+
+% corr_g1 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_sci_c.mat'))), [2 3 1]);
+% corr_g2 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_sci_t.mat'))), [2 3 1]);
 
 % model settings 
 A_g1 = squeeze(num2cell(corr_g1,[1 2])); % adjacency/connectivity matrix
@@ -15,9 +18,10 @@ N = length(A_g1{1}); % number of nodes (same for both groups)
 T_g1 = size(corr_g1,3); % number of layers (subjects)
 T_g2 = size(corr_g2,3);
 
-% multi-layer modularity (Q) and partiotions
+% multi-layer modularity (Q) and partitions
 % Group 1
-[B_g1,twom_g1] = multicat(A_g1,gamma,omega);
+[B_g1, twom_g1] = multicat(A_g1,gamma,omega);
+% [B_g1, twom_g1] = multicatdir_f(A_g1,gamma,omega);
 PP_g1 = @(S_g1)postprocess_categorical_multilayer(S_g1,T_g1);
 [S_g1,Q_g1] = iterated_genlouvain(B_g1,10000,0,1,'moverandw',[], PP_g1); % 4th entry(randord): 0[move] or 1[moverand] | 5th: move, moverand, or moverandw
 Q_g1 = Q_g1/twom_g1;
@@ -29,11 +33,15 @@ K_g1 = max(S_g1,[],'all'); % number of communities
 PP_g2 = @(S_g2)postprocess_categorical_multilayer(S_g2,T_g2);
 [S_g2,Q_g2] = iterated_genlouvain(B_g2,10000,0,1,'moverandw',[], PP_g2); % 4th entry(randord): 0[move] or 1[moverand] | 5th: move, moverand, or moverandw
 Q_g2 = Q_g2/twom_g2;
-S_g2 = reshape(S_g2,N,T_g2);
+S_g2 = reshape(S_g2, N, T_g2);
 C_g2 = mode(S_g2,2); % consensus
 K_g2 = max(S_g2,[],'all'); % number of communities
 
-cd '/Volumes/Elements/SCI/modularity_var/';           
-filename = sprintf('S_sci_c_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g1');
-filename = sprintf('S_sci_t_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g2');
+cd '/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/modularity_var/';
+
+filename = sprintf('S_hc_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g1');
+filename = sprintf('S_sci_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g2');
+
+% filename = sprintf('S_sci_c_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g1');
+% filename = sprintf('S_sci_t_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g2');
 
