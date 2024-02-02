a75f7d92eb5cb7383cb9ef57cb949c6268e78df7
ismailukman <ismailukman@gmail.com>
2024-02-02 09:39:37 -0500

    with robustness and concensus

---
+++ ModularityConsensus.m
@@ -0,0 +1,67 @@
+clear
+clc
+
+corr_g1 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_hc.mat'))), [2 3 1]);
+corr_g2 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_sci.mat'))), [2 3 1]);
+
+tic
+n = 50;
+
+
+% m_g1 = zeros(1, n);
+% m_g2 = zeros(1, n);
+m_g1 = cell(1, n);
+m_g2 = cell(1, n);
+
+gamma = 1.2; %1.2 gamma = 0.5:0.1:1.5
+log_omega = -1; %-1 log_omega = 0:-1:-4
+omega = power(10,log_omega);  
+
+parfor i = 1:n
+
+    % Use temporary variables for x and y inside parfor
+    count=1;
+    A_g1 = squeeze(num2cell(corr_g1, [1 2]));
+    A_g2 = squeeze(num2cell(corr_g2, [1 2]));
+    N = length(A_g1{1});
+    T_g1 = size(corr_g1, 3);
+    T_g2 = size(corr_g2, 3);
+
+    [B_g1, twom_g1] = multicat(A_g1,gamma,omega);
+    PP_g1 = @(S_g1)postprocess_categorical_multilayer(S_g1, T_g1);
+    [S_g1, Q_g1] = iterated_genlouvain(B_g1, 10000, 0, 1, 'moverandw', [], PP_g1);
+    Q_g1 = Q_g1 / twom_g1;
+
+    S_g1 = reshape(S_g1, N, T_g1);
+    C_g1 = mode(S_g1, 2);
+    K_g1 = max(S_g1, [], 'all');
+
+    [B_g2, twom_g2] = multicat(A_g2, gamma, omega);
+    PP_g2 = @(S_g2)postprocess_categorical_multilayer(S_g2, T_g2);
+    [S_g2, Q_g2] = iterated_genlouvain(B_g2, 10000, 0, 1, 'moverandw', [], PP_g2);
+    Q_g2 = Q_g2 / twom_g2;
+
+    S_g2 = reshape(S_g2, N, T_g2);
+    C_g2 = mode(S_g2, 2);
+    K_g2 = max(S_g2, [], 'all');
+    
+    count = count+1;
+    m_g1{i} = S_g1;
+    m_g2{i} = S_g2;
+    fprintf('%d. ',count);        
+end
+
+S_g1_m = cat(3, m_g1{:});
+S_g2_m = cat(3, m_g1{:});
+
+S_g1_mode = mode(S_g1_m, 3);
+S_g2_mode = mode(S_g2_m, 3);
+  
+toc
+
+cd '/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/modularity_var/';
+
+filename = sprintf('Sm_hc_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g1_mode');
+filename = sprintf('Sm_sci_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g2_mode');
+
+fprintf('Done!\n');
--- modularity_g2.m
+++ modularity_g2.m
@@ -1,16 +1,17 @@
 clear
 clc
-        
-gamma = 1.2; % gamma = 0.5:0.1:1.5
-log_omega = -1; % log_omega = 0:-1:-4
+
+gamma = 1.21; %1.2 gamma = 0.5:0.1:1.5
+log_omega = -1; %-1 log_omega = 0:-1:-4
 omega = power(10,log_omega);        
 
-% corr_g1 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_hc.mat'))), [2 3 1]);
-% corr_g2 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_sci.mat'))), [2 3 1]);
+% corr_all_g1 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_hc.mat'))), [2 3 1]);
+% corr_all_g2 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_sci.mat'))), [2 3 1]);
 
 corr_g1 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_sci_c.mat'))), [2 3 1]);
 corr_g2 = permute(cell2mat(struct2cell(load('/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/fc/corr_sci_t.mat'))), [2 3 1]);
 
+%%%% SCI
 % model settings 
 A_g1 = squeeze(num2cell(corr_g1,[1 2])); % adjacency/connectivity matrix
 A_g2 = squeeze(num2cell(corr_g2,[1 2]));         
@@ -24,7 +25,9 @@ T_g2 = size(corr_g2,3);
 % [B_g1, twom_g1] = multicatdir_f(A_g1,gamma,omega);
 PP_g1 = @(S_g1)postprocess_categorical_multilayer(S_g1,T_g1);
 [S_g1,Q_g1] = iterated_genlouvain(B_g1,10000,0,1,'moverandw',[], PP_g1); % 4th entry(randord): 0[move] or 1[moverand] | 5th: move, moverand, or moverandw
-Q_g1 = Q_g1/twom_g1;
+% [S_g1,Q_g1] = iterated_genlouvain(B_g1,10000,0,1,[], PP_g1);
+Q_g1 = Q_g1/twom_g1; % Community Modularity
+% fprintf('Modularity Group 1: %d\n', Q_g1);
 S_g1 = reshape(S_g1,N,T_g1);
 C_g1 = mode(S_g1,2); % consensus
 K_g1 = max(S_g1,[],'all'); % number of communities
@@ -32,9 +35,11 @@ K_g1 = max(S_g1,[],'all'); % number of communities
 [B_g2,twom_g2] = multicat(A_g2,gamma,omega);
 PP_g2 = @(S_g2)postprocess_categorical_multilayer(S_g2,T_g2);
 [S_g2,Q_g2] = iterated_genlouvain(B_g2,10000,0,1,'moverandw',[], PP_g2); % 4th entry(randord): 0[move] or 1[moverand] | 5th: move, moverand, or moverandw
-Q_g2 = Q_g2/twom_g2;
+% [S_g2,Q_g2] = iterated_genlouvain(B_g2,10000,0,1,[], PP_g2);
+Q_g2 = Q_g2/twom_g2; % Community Modularity
+% fprintf('Modularity Group 2: %d', Q_g2);
 S_g2 = reshape(S_g2, N, T_g2);
-C_g2 = mode(S_g2,2); % consensus
+C_g2 = mode(S_g2, 2); % consensus
 K_g2 = max(S_g2,[],'all'); % number of communities
 
 %       'moverandw': move the node under consideration to a community chosen
@@ -48,7 +53,8 @@ cd '/Users/ismaila/Documents/C-Codes/SCI_GraphTheory/sci_data/SCI/modularity_var
 
 % filename = sprintf('S_hc_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g1');
 % filename = sprintf('S_sci_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g2');
-
+ 
 filename = sprintf('S_sci_c_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g1');
 filename = sprintf('S_sci_t_%.1f,%.1f.mat', gamma, log_omega); save(filename,'S_g2');
+
 
